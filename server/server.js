da72d21638ee60776fa44dce99359e9121a70785
AMY LOUISE TUCK <amyltuck@hotmail.com>
2026-01-17 18:59:20 +0000

    fix: change endpoints for get and post requests back

--- server/server.js
+++ server/server.js
@@ -17,33 +17,27 @@ app.get("/", (req, res) => {
   res.json({ message: "Server is up and running!" });
 });
 
-app.get(
-  "https://teched-week-7-assignment-server.onrender.com/recipes",
-  async (req, res) => {
-    try {
-      const recipesData = await dbPool.query("SELECT * FROM wk7posts_recipes");
-      res.json(recipesData);
-    } catch (error) {
-      console.error(error);
-    }
-  },
-);
+app.get("/recipes", async (req, res) => {
+  try {
+    const recipesData = await dbPool.query("SELECT * FROM wk7posts_recipes");
+    res.json(recipesData);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-app.post(
-  "https://teched-week-7-assignment-server.onrender.com/new-recipe",
-  (req, res) => {
-    try {
-      const newRecipeData = req.body;
-      const query = dbPool.query(
-        `INSERT INTO wk7posts_recipes (recipe_name, ingredients, instructions) VALUES ($1, $2, $3)`,
-        [
-          newRecipeData.recipe_name,
-          newRecipeData.ingredients,
-          newRecipeData.instructions,
-        ],
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  },
-);
+app.post("/new-recipe", (req, res) => {
+  try {
+    const newRecipeData = req.body;
+    const query = dbPool.query(
+      `INSERT INTO wk7posts_recipes (recipe_name, ingredients, instructions) VALUES ($1, $2, $3)`,
+      [
+        newRecipeData.recipe_name,
+        newRecipeData.ingredients,
+        newRecipeData.instructions,
+      ],
+    );
+  } catch (error) {
+    console.error(error);
+  }
+});
